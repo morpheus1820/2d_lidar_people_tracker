a130b4b5c4c2c00262972874e8f174d88acefe74
morpheus1820 <rosa.stefano@gmail.com>
2024-02-26 15:51:00 +0000

    checkpoint path relative to package

--- detectors_ros/launch/dr_spaam_ros.launch.py
+++ detectors_ros/launch/dr_spaam_ros.launch.py
@@ -1,21 +1,36 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
     ld = LaunchDescription()
+    
+    ckpt_path = os.path.join(get_package_share_directory('detectors_ros'), 'checkpoints', 'ckpt_jrdb_ann_dr_spaam_e20.pth')
+    print(ckpt_path)
+    
     config = os.path.join(
         get_package_share_directory('detectors_ros'),
         'config',
         'dr_spaam_ros.yaml'
         )
-        
+		
+    param_substitutions = {
+        'weight_file': ckpt_path}
+	
+    configured_params = RewrittenYaml(
+        source_file=config,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+
+       
     node=Node(
         package = 'detectors_ros',
         executable = 'dr_spaam_ros',
-        parameters = [config]
+        parameters = [param_substitutions]
     )
     ld.add_action(node)
     return ld
