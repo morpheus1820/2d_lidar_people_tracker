c39c3d88404778139c7e6e26703533f503649bf9
Stefano Rosa <rosa.stefano@gmail.com>
2024-04-24 15:16:12 +0200

    Added alternate script for outlier removal

---
+++ tracker_ros/tracker_ros/remove_outliers_rpi_2.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np
+import os
+import rclpy
+import sys
+
+from copy import deepcopy
+from geometry_msgs.msg import PoseArray
+from nav_msgs.msg import OccupancyGrid
+
+from rclpy.node import Node
+
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+
+class OutlierRemoverStatic(Node):
+    def __init__(self):
+        super().__init__("outlier_remover_static_node")
+
+        self.yolo_poses = None
+        self.outlier_poses = []
+        self.counter = 0
+        
+
+        self.dr_spaam_sub = self.create_subscription(
+                    PoseArray, 
+                    'dr_spaam_detections', 
+                    self.dr_spaam_callback, 
+                    10
+                )
+
+        self.yolo_sub = self.create_subscription(
+                    PoseArray, 
+                    'yolo_detections', 
+                    self.yolo_callback, 
+                    10
+                )
+        
+        self.back_subtraction_sub = self.create_subscription(
+                    PoseArray, 
+                    'back_sub_dets', 
+                    self.back_sub_callback, 
+                    10
+                )
+
+        self.inliers_pub = self.create_publisher(
+            PoseArray, "inliers", 10
+        )
+        self.back_sub_inliers_pub = self.create_publisher(
+            PoseArray, "inliers_sub", 10
+        )
+        
+    def yolo_callback(self, msg):
+        poses = self.remove_outliers(msg)
+        if poses is not None:
+            self.inliers_pub.publish(poses)
+
+    def dr_spaam_callback(self, msg):
+        poses = self.remove_outliers(msg)
+        if poses is not None:
+            self.inliers_pub.publish(poses)
+
+    def back_sub_callback(self, msg):
+        poses = self.remove_outliers(msg)
+        if poses is not None:
+            self.back_sub_inliers_pub.publish(poses)
+
+    def remove_outliers(self, msg):
+        pose_array = PoseArray()
+        for pose in msg.poses:
+            p = np.array([pose.position.x, pose.position.y])
+
+            # if creating outliers list
+            if self.counter < 50:
+                self.outlier_poses.append(p)
+            else:
+                is_outlier = False
+                for op in self.outlier_poses:
+                    if np.linalg.norm(op-p) < 0.3:
+                        is_outlier = True
+                        break 
+                
+                if not is_outlier:
+                    pose_array.poses.append(pose)
+
+        self.counter += 1
+        # convert to ros msg and publish
+        pose_array.header = msg.header
+        return pose_array
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = OutlierRemoverStatic()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
