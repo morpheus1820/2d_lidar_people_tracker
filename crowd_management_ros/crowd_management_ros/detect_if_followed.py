3f37770be3bb8a3b6cd099bc45931568ec53a22f
stefano rosa <morpheus@stefanos-Laptop.local>
2024-04-30 11:57:00 +0200

    publish true if robot turning

--- crowd_management_ros/crowd_management_ros/detect_if_followed.py
+++ crowd_management_ros/crowd_management_ros/detect_if_followed.py
@@ -4,6 +4,7 @@ import rclpy
 import sys
 
 from geometry_msgs.msg import Pose, PoseArray
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
 from rclpy.parameter import Parameter
 from rclpy.duration import Duration
@@ -17,17 +18,20 @@ class FollowingGroupDetector(Node):
     def __init__(self):
         super().__init__("following_group_detector_node")
 
-        self.moving_avg_group = None # format: centerx, centery, convariance in robot frame 
+        self.moving_avg_group = None # format: centerx, centery, convariance in robot frame
         self.count = 0
         self.is_followed_array = np.zeros(50)
         self.b, self.a = signal.butter(3, 0.05)
         self.zi = signal.lfilter_zi(self.b, self.a)
+        self.rot_vel = 0.0
 
         # subscribers
         self.inliers_sub = self.create_subscription(
             PoseArray, "inliers", self.inliers_callback, 10
         )
-        
+        self.odom_sub = self.create_subscription(
+            Odometry, "odometry", self.odom_callback, 10
+        )
         #publishers
         self.marker_pub = self.create_publisher(
             MarkerArray, "detected_group_marker", 10
@@ -38,30 +42,39 @@ class FollowingGroupDetector(Node):
         self.is_followed_filtered_pub = self.create_publisher(
             Bool, "is_followed_filtered", 10)
 
-    def inliers_callback(self, msg):            
-        group_poses = []    
+    def odom_callback(self, msg):
+        self.rot_vel = msg.pose.twist.twist.angular.z
+
+    def inliers_callback(self, msg):
+        # if robot is turning
+        if self.rot_vel > 0.1:
+            is_followed = Bool()
+            is_followed.data = True
+            return
+
+        group_poses = []
         group_center = None
         group_covariance = None
         is_followed = Bool()
         is_followed.data = False
-        
+
         for pose in msg.poses:
             x = pose.position.x
             y = pose.position.y
             if x < -0.5 and x > -6 and np.abs(y) < 3:
                 group_poses.append([x,y])
-        
-        
-        if len(group_poses) > 1:   
-        
+
+
+        if len(group_poses) > 1:
+
             is_followed.data = True
-            
+
             group_center = np.mean(group_poses,axis=0)
-            group_covariance = np.max(np.std(group_poses,axis=0))    
+            group_covariance = np.max(np.std(group_poses,axis=0))
             #print(group_covariance)
             if group_covariance > 0.6:
                 group_covariance = 0.6
-             
+
             if self.moving_avg_group is not None:
                 self.moving_avg_group[:2] = (self.moving_avg_group[:2] + group_center) / 2
                 self.moving_avg_group[2] = (self.moving_avg_group[2] + group_covariance) / 2
@@ -69,15 +82,15 @@ class FollowingGroupDetector(Node):
                 self.moving_avg_group = np.zeros(3)
                 self.moving_avg_group[:2] = group_center
                 self.moving_avg_group[2] = group_covariance
-                
+
             #visualization
             marker_msg = MarkerArray()
-            
+
             # circle
             r = 0.1
             ang = np.linspace(0, 2 * np.pi, 20)
             xy_offsets = r * np.stack((np.cos(ang), np.sin(ang)), axis=1)
-                                     
+
             marker = Marker()
             marker.id = 1
             marker.lifetime = Duration(seconds=1.0).to_msg()
@@ -88,22 +101,22 @@ class FollowingGroupDetector(Node):
             marker.scale.x = self.moving_avg_group[2] * 6
             marker.scale.y = self.moving_avg_group[2] * 6
             marker.scale.z = self.moving_avg_group[2] * 6
-            
+
             marker.color.a = 0.3
             marker.color.r = 1.0
             marker.color.g = 1.0
             marker.color.b = 0.0
-            
+
             marker.pose.orientation.w = 1.0
             marker.pose.position.x = self.moving_avg_group[0]
             marker.pose.position.y = self.moving_avg_group[1]
             marker.pose.position.z = 0.0
             marker_msg.markers.append(marker)
-                       
+
             self.marker_pub.publish(marker_msg)
-        
-        self.is_followed_pub.publish(is_followed)   
-	
+
+        self.is_followed_pub.publish(is_followed)
+
         # filter
         if self.count % 10 == 0:
                 self.is_followed_array = np.roll(self.is_followed_array, 1)
@@ -113,19 +126,19 @@ class FollowingGroupDetector(Node):
 
                 is_followed = Bool()
                 is_followed.data = True if np.mean(filtered) > 0.2 else False
-                self.is_followed_filtered_pub.publish(is_followed) 
-                print("mean", np.mean(filtered))                  
-                        
-                        
+                self.is_followed_filtered_pub.publish(is_followed)
+                print("mean", np.mean(filtered))
+
+
 #                print(f"{self.is_followed_array=}")
 #                print(f"{filtered=}")
-                
+
 #                plt.close('all')
 #                plt.plot(self.is_followed_array)
 #                plt.plot(filtered)
-#                plt.show() 
+#                plt.show()
         self.count += 1
-        
+
 
 def main(args=None):
     rclpy.init(args=args)
