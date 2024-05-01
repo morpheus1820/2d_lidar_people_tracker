b7b3dacf34d3047cf32951951d7647561a0ecb4b
Stefano Rosa <rosa.stefano@gmail.com>
2024-05-01 21:08:47 +0200

    Added timeout topic

--- crowd_management_ros/crowd_management_ros/detect_if_followed.py
+++ crowd_management_ros/crowd_management_ros/detect_if_followed.py
@@ -18,6 +18,7 @@ class FollowingGroupDetector(Node):
         super().__init__("following_group_detector_node")
 
         self.inliers = None
+        self.odometry = None
         self.moving_avg_group = None # format: centerx, centery, convariance in robot frame
         self.count = 0
         
@@ -45,105 +46,123 @@ class FollowingGroupDetector(Node):
         self.is_followed_unfiltered_pub = self.create_publisher(
             Bool, "is_followed_unfiltered", 10
         )
-
+        self.is_followed_timeout_pub = self.create_publisher(
+            Bool, "is_followed_timeout", 10
+        )
+        
         # timer
-        self.check_following_timer = self.create_timer(.1, self.check_following_callback)
+        self.check_following_timer = self.create_timer(1.0, self.check_following_callback)
 
     def odom_callback(self, msg):
-        self.rot_vel = msg.twist.twist.angular.z
-
+        self.odometry = msg
+        
     def inliers_callback(self, msg):
         self.inliers = msg
         
     def check_following_callback(self):
-        if self.inliers is None:
-            return
-            
-        # if robot is turning
-        if self.rot_vel > 0.2:
-            is_followed = Bool()
-            is_followed.data = True
-            return
-
         group_poses = []
         group_center = None
         group_covariance = None
+                
+        # if robot is turning
+        if self.odometry is not None and self.odometry.twist.twist.angular.z > 0.2:
+            is_followed_unfiltered = Bool()
+            is_followed_unfiltered.data = True
+            self.is_followed_unfiltered_pub.publish(is_followed_unfiltered)
+            return
+
+        # init followed state = False
         is_followed_unfiltered = Bool()
         is_followed_unfiltered.data = False
 
-        for pose in self.inliers.poses:
-            x = pose.position.x
-            y = pose.position.y
-            if x < -0.5 and x > -6 and np.abs(y) < 3:
-                group_poses.append([x,y])
+        # if any inliers were received, detect group
+        if self.inliers is None:
+            time_diff = 1e6
+        else:
+            time_diff = self.odometry.header.stamp.sec - self.inliers.header.stamp.sec
+            
+        print(f"{time_diff=}")
+        
+        if self.inliers is not None and time_diff < 10:
+            for pose in self.inliers.poses:
+                x = pose.position.x
+                y = pose.position.y
+                if x < -0.5 and x > -6 and np.abs(y) < 3:
+                    group_poses.append([x,y])
+    
+            # if group detected
+            if len(group_poses) > 1:
+                # set followed state = True
+                is_followed_unfiltered.data = True
+    
+                # estimate group position, covariance
+                group_center = np.mean(group_poses,axis=0)
+                group_covariance = np.max(np.std(group_poses,axis=0))
+    
+                if group_covariance > 0.6:
+                    group_covariance = 0.6
+    
+                if self.moving_avg_group is not None:
+                    self.moving_avg_group[:2] = (self.moving_avg_group[:2] + group_center) / 2
+                    self.moving_avg_group[2] = (self.moving_avg_group[2] + group_covariance) / 2
+                else:
+                    self.moving_avg_group = np.zeros(3)
+                    self.moving_avg_group[:2] = group_center
+                    self.moving_avg_group[2] = group_covariance
+    
+                # visualization 
+                marker_msg = MarkerArray()
+    
+                # circle
+                r = 0.1
+                ang = np.linspace(0, 2 * np.pi, 20)
+                xy_offsets = r * np.stack((np.cos(ang), np.sin(ang)), axis=1)
+    
+                marker = Marker()
+                marker.id = 1
+                marker.lifetime = Duration(seconds=1.0).to_msg()
+                marker.header.stamp = self.inliers.header.stamp
+                marker.header.frame_id = "mobile_base_body_link"
+                marker.type = marker.SPHERE
+                marker.action = marker.ADD
+                marker.scale.x = self.moving_avg_group[2] * 6
+                marker.scale.y = self.moving_avg_group[2] * 6
+                marker.scale.z = self.moving_avg_group[2] * 6
+    
+                marker.color.a = 0.3
+                marker.color.r = 1.0
+                marker.color.g = 1.0
+                marker.color.b = 0.0
+    
+                marker.pose.orientation.w = 1.0
+                marker.pose.position.x = self.moving_avg_group[0]
+                marker.pose.position.y = self.moving_avg_group[1]
+                marker.pose.position.z = 0.0
+                marker_msg.markers.append(marker)
+    
+                self.marker_pub.publish(marker_msg)
 
-        if len(group_poses) > 1:
-
-            is_followed_unfiltered.data = True
-
-            group_center = np.mean(group_poses,axis=0)
-            group_covariance = np.max(np.std(group_poses,axis=0))
-
-            if group_covariance > 0.6:
-                group_covariance = 0.6
-
-            if self.moving_avg_group is not None:
-                self.moving_avg_group[:2] = (self.moving_avg_group[:2] + group_center) / 2
-                self.moving_avg_group[2] = (self.moving_avg_group[2] + group_covariance) / 2
-            else:
-                self.moving_avg_group = np.zeros(3)
-                self.moving_avg_group[:2] = group_center
-                self.moving_avg_group[2] = group_covariance
-
-            #visualization
-            marker_msg = MarkerArray()
-
-            # circle
-            r = 0.1
-            ang = np.linspace(0, 2 * np.pi, 20)
-            xy_offsets = r * np.stack((np.cos(ang), np.sin(ang)), axis=1)
-
-            marker = Marker()
-            marker.id = 1
-            marker.lifetime = Duration(seconds=1.0).to_msg()
-            marker.header.stamp = self.inliers.header.stamp
-            marker.header.frame_id = "mobile_base_body_link"
-            marker.type = marker.SPHERE
-            marker.action = marker.ADD
-            marker.scale.x = self.moving_avg_group[2] * 6
-            marker.scale.y = self.moving_avg_group[2] * 6
-            marker.scale.z = self.moving_avg_group[2] * 6
-
-            marker.color.a = 0.3
-            marker.color.r = 1.0
-            marker.color.g = 1.0
-            marker.color.b = 0.0
-
-            marker.pose.orientation.w = 1.0
-            marker.pose.position.x = self.moving_avg_group[0]
-            marker.pose.position.y = self.moving_avg_group[1]
-            marker.pose.position.z = 0.0
-            marker_msg.markers.append(marker)
-
-            self.marker_pub.publish(marker_msg)
-
+        # publish followed state
         self.is_followed_unfiltered_pub.publish(is_followed_unfiltered)
 
-        # filter
-        if self.count % 10 == 0:
-                self.is_followed_array = np.roll(self.is_followed_array, 1)
-                self.is_followed_array[0] = float(is_followed_unfiltered.data)
-                filtered, _ = signal.lfilter(self.b, self.a, self.is_followed_array, zi=self.zi*self.is_followed_array[0])
-                filtered, _ = signal.lfilter(self.b, self.a, filtered, zi=self.zi*self.is_followed_array[0])
-                
-                self.count = 0
-
-                is_followed = Bool()
-                is_followed.data = True if np.mean(filtered) > 0.1 else False
-                self.is_followed_pub.publish(is_followed)
-                print("mean", np.mean(filtered))
-
-        self.count += 1
+        # add followed state to window
+        self.is_followed_array = np.roll(self.is_followed_array, 1)
+        self.is_followed_array[0] = float(is_followed_unfiltered.data)
+        
+        # filtering
+        filtered, _ = signal.lfilter(self.b, self.a, self.is_followed_array, zi=self.zi*self.is_followed_array[0])
+        filtered, _ = signal.lfilter(self.b, self.a, filtered, zi=self.zi*self.is_followed_array[0])
+        
+        # publish filtered followed state
+        is_followed = Bool()
+        is_followed.data = True if np.mean(filtered) > 0.2 else False
+        self.is_followed_pub.publish(is_followed)
+        print("mean", np.mean(filtered))
+        
+        # publish timeout state
+        is_followed_timeout = Bool()
+        is_followed_timeout.data = True if self.is_followed_array[:10].sum() > 1e-5 else False
+        self.is_followed_timeout_pub.publish(is_followed_timeout)
 
 def main(args=None):
     rclpy.init(args=args)
